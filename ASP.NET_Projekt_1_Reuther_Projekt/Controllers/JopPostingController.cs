f298c4a85876b0b862fc339032a87bf5b47d28d3
The-EverChosen <Meier-bewerbung@gmx.de>
2024-09-24 07:18:14 +0200

    -Das Model mit der Datenbank verbunden -CompanyImage funktional gemacht

--- ASP.NET_Projekt_1_Reuther_Projekt/Controllers/JopPostingController.cs
+++ ASP.NET_Projekt_1_Reuther_Projekt/Controllers/JopPostingController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP.NET_Projekt_1_Reuther_Projekt.Data;
+using ASP.NET_Projekt_1_Reuther_Projekt.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_Projekt_1_Reuther_Projekt.Controllers
 {
     public class JopPostingController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public JopPostingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var JopPostingsFromDb = _context.jobPostings.Where(x => x.OwnerUsername == User.Identity.Name).ToList();
+            return View(JopPostingsFromDb);
+
         }
 
         public IActionResult FormJopPosting()
@@ -16,6 +25,20 @@ namespace ASP.NET_Projekt_1_Reuther_Projekt.Controllers
 
         public IActionResult createdJobPosting()
         {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult CreatedEditJop(JopPost jopPost, IFormFile file)
+        {
+            if (file != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    var bytes = ms.ToArray();
+                    jopPost.CompanyImage = bytes;
+                }
+            }
             return RedirectToAction("Index");
         }
     }
